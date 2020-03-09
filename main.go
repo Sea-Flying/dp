474cb120ec7a955e4ddb2aef994063b54f581972
seaflying <seaflying@outlook.com>
2020-03-10 00:24:41 +0800

    临睡前提交一点>_<.

--- main.go
+++ main.go
@@ -5,4 +5,9 @@ import (
 	"github.com/gocql/gocql"
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/scylladb/gocqlx"
+	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
+
+func main() {
+	gitlab.WebhookOptions{}
+}
