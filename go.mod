246369092882e1a448e864eae4315f11c6f0159b
Jonas Gao <jonas.gao@voyageone.cn>
2020-09-25 14:48:10 +0800

    feat: add cors support

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module voyageone.com/dp
 go 1.14
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-git/go-git/v5 v5.1.0
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
--- infrastructure/initialize/initRouter.go
+++ infrastructure/initialize/initRouter.go
@@ -1,12 +1,14 @@
 package initialize
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"voyageone.com/dp/router/v1"
 )
 
 func InitRouter() (Router *gin.Engine) {
 	Router = gin.Default()
+	Router.Use(cors.Default())
 	router.InitApiRouter(Router)
 	return
 }
